01e796012678886bf3423de8a4b426c00d1a660a
jsbooth-git <jacob.booth.tkd99@gmail.com>
2022-02-28 11:54:59 +1100

    Began inserting the popup cards section

---
+++ Home/script.js
@@ -0,0 +1,26 @@
+// ------------------------------ POPUP CARD SCRIPT ------------------------- //
+
+var popupViews = document.querySelectorAll('.popup-view');
+var popupBtns = document.querySelectorAll('.popup-btn');
+var closeBtns = document.querySelectorAll('.fa-xmark');
+
+// View button functionality
+
+var popup = function (popupClick) {
+  popupViews[popupClick].classList.add('active');
+};
+
+popupBtns.forEach((popupBtn, i) => {
+  popupBtn.addEventListener('click', () => {
+    popup(i);
+  });
+});
+
+// Close button functionality
+closeBtns.forEach((closeBtn) => {
+  closeBtn.addEventListener('click', () => {
+    popupViews.forEach((popupView) => {
+      popupView.classList.remove('active');
+    });
+  });
+});
